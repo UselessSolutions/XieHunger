97bf685c49f8fc2c380a7ec7856988f10548c406
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2023-11-15 00:15:39 -0600

    Food mixin

---
+++ src/main/java/useless/xiehunger/mixin/ItemFoodMixin.java
@@ -0,0 +1,23 @@
+package useless.xiehunger.mixin;
+
+import net.minecraft.core.entity.player.EntityPlayer;
+import net.minecraft.core.item.ItemFood;
+import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import useless.xiehunger.XieHunger;
+
+@Mixin(value = ItemFood.class, remap = false)
+public class ItemFoodMixin {
+	@Shadow
+	protected int healAmount;
+
+	@Inject(method = "onItemRightClick(Lnet/minecraft/core/item/ItemStack;Lnet/minecraft/core/world/World;Lnet/minecraft/core/entity/player/EntityPlayer;)Lnet/minecraft/core/item/ItemStack;", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/entity/player/EntityPlayer;heal(I)V"))
+	private void healHunger(ItemStack itemstack, World world, EntityPlayer entityplayer, CallbackInfoReturnable<ItemStack> cir){
+		XieHunger.feed(healAmount, (ItemFood)(Object)this);
+	}
+}
